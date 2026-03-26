c79cac35f767188d3473c4a9ed48a61c963a146c
Rinku-Digilantern <rinku.raghuwanshi@digilantern.in>
2026-03-26 13:07:50 +0530

    thanyoupage changes

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -3,10 +3,6 @@ import React from 'react'
 import Link from 'next/link'
 import styles from './style.module.css'
 import { AiFillCheckCircle } from 'react-icons/ai'
-import type { Metadata } from "next";
-import metadataConfig from "@/app/metadata";
-export const generateMetadata = (): Metadata => metadataConfig["/thank-you"];
-
 
 const API_BASE = 'https://api.citrineclinic.com/api';
 
@@ -45,7 +41,6 @@ export default async function ThankYou() {
 
   return (
     <>
-
       {seo?.faq_schema && (
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(seo.faq_schema) }} />
       )}
@@ -53,17 +48,18 @@ export default async function ThankYou() {
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(seo.bred_schema) }} />
       )}
 
-    <section className={styles.thankpage}>
-      <div className="wrapper">
-        <div className={`row ${styles.row}`}>
-          <div className={`col col-12 col-md-8 col-lg-7 ${styles.col}`}>
-            <AiFillCheckCircle/>
-            <h1 className={`mainHeading ${styles.mainHeading}`}>Thank You!</h1>
-            <p className="para">We have received your request. We will get in touch with you shortly.</p>
-            <Link href="/" className={styles.gohome}>Go Home</Link>
+      <section className={styles.thankpage}>
+        <div className="wrapper">
+          <div className={`row ${styles.row}`}>
+            <div className={`col col-12 col-md-8 col-lg-7 ${styles.col}`}>
+              <AiFillCheckCircle/>
+              <h1 className={`mainHeading ${styles.mainHeading}`}>Thank You!</h1>
+              <p className="para">We have received your request. We will get in touch with you shortly.</p>
+              <Link href="/" className={styles.gohome}>Go Home</Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 }
