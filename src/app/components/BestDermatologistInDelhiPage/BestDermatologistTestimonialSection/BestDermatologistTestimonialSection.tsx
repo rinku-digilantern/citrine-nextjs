8f367707d13979d0b249f3338247b3899ea44dcf
Rinku-Digilantern <rinku.raghuwanshi@digilantern.in>
2026-04-24 10:44:07 +0530

    blog page margin and heading tag font size

--- src/app/components/BestDermatologistInDelhiPage/BestDermatologistTestimonialSection/BestDermatologistTestimonialSection.tsx
+++ src/app/components/BestDermatologistInDelhiPage/BestDermatologistTestimonialSection/BestDermatologistTestimonialSection.tsx
@@ -22,14 +22,14 @@ const BestDermatologistTestimonialSection: React.FC<Props> = ({ section }) => {
 
   return (
     <section className={styles.BestDermatologistTestimonialSection}>
-      <Image src="/assets/images/home/patienttestimonialbg.webp" alt="Testimonial Background" fill className={styles.backgroundImage}/>
+      <Image src="/assets/images/home/patienttestimonialbg.webp" alt="Testimonial Background" fill className={styles.backgroundImage} />
       <div className={styles.container}>
         <h2 className={styles.mainHeading}>{section.mainHeading}</h2>
         <p className={styles.desc}>{section.paragraph}</p>
         <div className={styles.testimonialsGrid}>
           {section.testimonials.map((testimonial) => (
             <div key={testimonial.id} className={styles.testimonialCard}>
-              <h3 className={styles.cardTitle}>{testimonial.title}</h3>
+              {/* <h3 className={styles.cardTitle}>{testimonial.title}</h3> */}
               <div className={styles.stars}>
                 {[...Array(testimonial.rating)].map((_, index) => (
                   <FaStar key={index} className={styles.star} />
@@ -40,13 +40,13 @@ const BestDermatologistTestimonialSection: React.FC<Props> = ({ section }) => {
                 ✱ Opinions/Results may vary from person to person.
               </div>
               <div className={styles.servicefooter}>
-                <p className={styles.author}>—— {testimonial.author}</p>
+                <p className={styles.author}>— {testimonial.author}</p>
                 <div className={styles.googleIcon}>
-                  <Image 
-                    src={testimonial.google || "/assets/images/serviceinnerpage/google.webp"} 
-                    alt="Google" 
-                    width={28} 
-                    height={28}/>
+                  <Image
+                    src={testimonial.google || "/assets/images/serviceinnerpage/google.webp"}
+                    alt="Google"
+                    width={28}
+                    height={28} />
                 </div>
               </div>
             </div>
