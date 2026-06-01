5e4cf71e902081bd63653e16b514a0c1a6695b8a
Rinku-Digilantern <rinku.raghuwanshi@digilantern.in>
2026-06-01 15:56:32 +0530

    logo changes and drug term remove

--- src/app/components/BestDermatologistInDelhiPage/DermatologyThirdSection/DermatologyThirdSection.tsx
+++ src/app/components/BestDermatologistInDelhiPage/DermatologyThirdSection/DermatologyThirdSection.tsx
@@ -43,7 +43,7 @@ const DermatologyThirdSection: React.FC<Props> = ({ section }) => {
           {/* If FAQ data present, render FAQ */}
           <>
             {section.paragraph1 && <p dangerouslySetInnerHTML={{ __html: section.paragraph1 }} />}
-            <h3>{section.subHeading}</h3>
+            {section.subHeading && <h3>{section.subHeading}</h3>}
             {section.list && (
               <ul className={styles.BestDermatologistList}>
                 {section.list.map((item, idx) => (
@@ -51,7 +51,7 @@ const DermatologyThirdSection: React.FC<Props> = ({ section }) => {
                 ))}
               </ul>
             )}
-            <h3>{section.subHeadings}</h3>
+            {section.subHeadings && <h3>{section.subHeadings}</h3>}
             {section.lists && (
               <ul className={styles.BestDermatologistList}>
                 {section.lists.map((item, idx) => (
@@ -59,11 +59,11 @@ const DermatologyThirdSection: React.FC<Props> = ({ section }) => {
                 ))}
               </ul>
             )}
-            <h3>{section.subHeadingss}</h3>
+            {section.subHeadingss && <h3>{section.subHeadingss}</h3>}
             {section.paragraph3 && <p dangerouslySetInnerHTML={{ __html: section.paragraph3 }} />}
-            <h3>{section.subheading}</h3>
+            {section.subheading && <h3>{section.subheading}</h3>}
             {section.desc && <p dangerouslySetInnerHTML={{ __html: section.desc }} />}
-            <h3>{section.subhead}</h3>
+            {section.subhead && <h3>{section.subhead}</h3>}
             {section.descing && <p dangerouslySetInnerHTML={{ __html: section.descing }} />}
             {section.table && section.table.length > 0 && tableColumns.length > 0 && (
               <div className={styles.tableResponsive}>
