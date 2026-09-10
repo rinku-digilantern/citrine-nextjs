e7be95337a1fb2e80af978183b163b97fb0b0e42
Rinku-Digilantern <rinku.raghuwanshi@digilantern.in>
2026-09-10 18:05:02 +0530

    Add GTM container GTM-WSKMW2TK alongside existing GTM-5R4CBF3

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,15 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
           }}
         />
 
+        {/* Google Tag Manager Script (GTM-WSKMW2TK) */}
+        <Script
+          id="gtm-script-wskmw2tk"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function (w, d, s, l, i) { w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' }); var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f); })(window, document, 'script', 'dataLayer', 'GTM-WSKMW2TK');`,
+          }}
+        />
+
         {/* Organization Schema */}
         <Script
           id="organization-schema"
@@ -160,6 +169,17 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-5R4CBF3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {/* Google Tag Manager (noscript) - GTM-WSKMW2TK */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WSKMW2TK"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
